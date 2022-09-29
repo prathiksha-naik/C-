24f1fab67317f65a48a0c977f11b25c50c916044
prathiksha-naik <prathiksha07416@gmail.com>
2022-09-29 17:51:10 +0530

    Initial Commit

---
+++ MyProject1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject1
+{
+    internal class Class1
+    {
+        static void main(string[] a)
+        {
+            Console.WriteLine("Hello World!!");
+        }
+    }
+}
