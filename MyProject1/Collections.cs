8d48a92f5c922866df2235bd93ceaa40ef4a502b
prathiksha-naik <prathiksha07416@gmail.com>
2022-10-08 00:27:33 +0530

    commit

---
+++ MyProject1/Collections.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+class Collections {
+    public static void Main()
+    {
+
+        
+        DictionaryDemo();
+        HashtableDemo();
+
+
+    }
+    public static void DictionaryDemo()
+{
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+        dict.Add("  Prathiksha", "Developer  ");
+        dict.Add("  Prarthana", "Tester  ");
+        dict.Add("  Ankith", "Database  ");
+        Console.WriteLine("DICTIONARY");
+        foreach (KeyValuePair<string, string> kvp in dict)
+        {
+            Console.Write(kvp.Key.ToString() + " ");
+            Console.Write(kvp.Value.ToString());
+            Console.WriteLine();
+        }
+        
+
+    
+    Console.WriteLine("*********************************");
+
+}
+    public static void HashtableDemo()
+    {
+        Hashtable hashtable = new Hashtable();
+        hashtable.Add(1, "C#");
+        hashtable.Add(2, "Java");
+        hashtable.Add("Backend", "Python");
+        Console.WriteLine("HASHTABLE");
+       
+        ICollection key = hashtable.Keys;
+        foreach (var contents in key)
+        {
+            Console.WriteLine($"  {hashtable[contents]}  ");
+        }
+        
+
+    }
+}
---
+++ MyProject1/GenericDelegate.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System;
+using System.Drawing;
+
+class GenericDelegate
+{
+
+    
+    public delegate void circle(double rad);
+    public delegate void cube(double side);
+    public delegate void square(double side);
+
+    public void circlearea(double rad)
+    {
+        double area = Math.PI * rad * rad;
+        Console.WriteLine("Area of circle is: {0}",area);
+    }
+    public void cubearea(double side)
+    {
+        double Area = 6 * (side * side);
+        Console.WriteLine("Area of cube is: {0}", Area);
+    }
+    public void squarearea(double side)
+    {
+        double Area = side * side;
+        Console.WriteLine("Area of square is: {0}", Area);
+    }
+
+
+    
+
+    public static void Main(String[] args)
+    {
+        GenericDelegate rect = new GenericDelegate();
+        circle obj1 = new circle(rect.circlearea);
+        obj1.Invoke(2);
+        cube obj2 = new cube(rect.cubearea);
+        obj2.Invoke(4);
+        square obj3 = new square(rect.squarearea);
+        obj3.Invoke(6);
+
+
+
+
+    }
+}
+
